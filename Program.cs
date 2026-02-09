7efef06d2cfac185d48969366df39f163dccbbc3
Adunc64 <raheekraiyan@gmail.com>
2026-02-09 16:54:39 +0600

    Auto apply EF core migrations on startup

--- Program.cs
+++ Program.cs
@@ -27,6 +27,12 @@ builder.Services.AddSingleton<IEmailSender, ConsoleEmailSender>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<task.Data.AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
